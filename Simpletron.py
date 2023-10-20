23e7420bde270e398a423a74b853a0a66a87b93e
leouev <leouev@gmail.com>
2023-10-19 23:09:18 -0400

    added operations remainder,exponentiation,newline

--- Simpletron.py
+++ Simpletron.py
@@ -44,6 +44,12 @@ class Simpletron:
                 self.accumulator //= self.memory[operand]
             elif opcode == 33:
                 self.accumulator *= self.memory[operand]
+            elif self.operation_code == 34:  # REMAINDER
+                self.accumulator %= self.memory[operand]
+            elif self.operation_code == 35:  # EXPONENTIATION
+                self.accumulator **= self.memory[operand]
+            elif self.operation_code == 36:  # NEWLINE
+                print()  # Output a newline
             elif opcode == 40:
                 self.instruction_counter = operand
             elif opcode == 41:
